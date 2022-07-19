7cabe2cd3f145ee5addedb76f03bb0fc28760422
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-07-19 19:42:28 +0200

    Bump 1.16.5 forge version and improve code

--- src/main/java/de/cas_ual_ty/deuf/DEUF.java
+++ src/main/java/de/cas_ual_ty/deuf/DEUF.java
@@ -1,12 +1,5 @@
 package de.cas_ual_ty.deuf;
 
-import java.util.Collection;
-import java.util.Random;
-import java.util.UUID;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ClassInheritanceMultiMap;
@@ -16,6 +9,12 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collection;
+import java.util.Random;
+import java.util.UUID;
 
 @Mod(DEUF.MOD_ID)
 public class DEUF
@@ -36,30 +35,32 @@ public class DEUF
             return;
         }
         
-        Chunk chunk = (Chunk)event.getChunk();
-        if(!chunk.getWorldForge().isRemote() && chunk.getWorldForge() instanceof ServerWorld)
+        Chunk chunk = (Chunk) event.getChunk();
+        if(chunk.getWorldForge() != null && !chunk.getWorldForge().isClientSide && chunk.getWorldForge() instanceof ServerWorld)
         {
-            ServerWorld world = (ServerWorld)chunk.getWorldForge();
-            ClassInheritanceMultiMap<Entity>[] entities = chunk.getEntityLists();
-            UUID uuid;
-            UUID uuidNew;
-            Collection<Entity> entityCollection;
+            ServerWorld world = (ServerWorld) chunk.getWorldForge();
+            ClassInheritanceMultiMap<Entity>[] entities = chunk.getEntitySections();
+            
             for(ClassInheritanceMultiMap<Entity> classinheritancemultimap : entities)
             {
-                entityCollection = com.google.common.collect.ImmutableList.copyOf(classinheritancemultimap);
+                Collection<Entity> entityCollection = com.google.common.collect.ImmutableList.copyOf(classinheritancemultimap);
+                
                 for(Entity entity : entityCollection)
                 {
                     if(!(entity instanceof PlayerEntity))
                     {
-                        uuid = entity.getUniqueID();
-                        if(world.getEntityByUuid(uuid) != null && world.getEntityByUuid(uuid) != entity)
+                        UUID uuid = entity.getUUID();
+                        
+                        if(world.getEntity(uuid) != entity)
                         {
-                            uuidNew = MathHelper.getRandomUUID(new Random());
-                            while(world.getEntityByUuid(uuidNew) != null)
+                            UUID uuidNew = MathHelper.createInsecureUUID(new Random());
+                            while(world.getEntity(uuidNew) != null)
                             {
-                                uuidNew = MathHelper.getRandomUUID(new Random());
+                                uuidNew = MathHelper.createInsecureUUID(new Random());
                             }
-                            entity.setUniqueId(uuidNew);
+                            
+                            entity.setUUID(uuidNew);
+                            
                             DEUF.LOGGER.info("Changing UUID of entity {} that already existed from {} to {}", entity.getType().getRegistryName().toString(), uuid.toString(), uuidNew.toString());
                         }
                     }
